ff252e3b5dd7f611ba1f8540f907a263f02e8b41
Waldemar Liedle <waldemar.liedle@gmail.com>
2018-04-03 00:53:30 +0200

    added miner, transaction pool to p2pserver, postman requests

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require('./transaction');
+
 class TransactionPool {
 
     constructor() {
@@ -19,6 +21,26 @@ class TransactionPool {
 
     existingTransaction(address) {
         return this.transactions.find(t => t.input.address === address);
+    }
+
+    validTransactions() {
+        return this.transactions.filter(transaction => {
+            const outputTotal = transaction.outputs.reduce((total, output) => {
+                return total + output.amount;
+            }, 0);
+
+            if (transaction.input.amount !== outputTotal) {
+                console.log(`invalid transaction from ${transaction.input.address}`);
+                return;
+            }
+
+            if (!Transaction.verifyTransaction(transaction)) {
+                console.log(`invalid signature from ${transaction.input.address}`);
+                return;
+            }
+
+            return transaction;
+        });
     }
 
 }
