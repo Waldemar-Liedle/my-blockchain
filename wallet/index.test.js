d0a866c1f9a7d3eb43932d7e3e2601ba30bc8bc6
Waldemar Liedle <waldemar.liedle@gmail.com>
2018-04-03 11:33:52 +0200

    testing errors solved

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -12,12 +12,6 @@ describe('Wallet', () => {
         bc = new Blockchain();
     });
 
-    /*
-    it('bla', () => {
-        expect(true).toEqual(true);
-    });
-    */
-
     describe('creating a transaction', () => {
         let transaction, sendAmount, recipient;
 
@@ -32,50 +26,32 @@ describe('Wallet', () => {
                 wallet.createTransaction(recipient, sendAmount, bc, tp);
             });
 
-            it('doubles the sendAmount substracted from the wallet balance', () => {
-                expect(
-                    transaction.outputs
-                        .find(output => output.address === wallet.publicKey).amount
-                ).toEqual(wallet.balance - sendAmount * 2);
+            it('doubles the `sendAmount` subtracted from the wallet balance', () => {
+                expect(transaction.outputs.find(
+                    output => output.address === wallet.publicKey
+                ).amount).toEqual(wallet.balance - sendAmount * 2);
             });
 
-            it('clones the sendAmount output for the recipient', () => {
-                expect(
-                    transaction.outputs.filter(
-                        output => output.address === recipient
-                    ).map(output => output.amount)
-                ).toEqual([sendAmount, sendAmount]);
+            it('clones the `sendAmount` output for the recipient', () => {
+                expect(transaction.outputs.filter(output => output.address === recipient)
+                    .map(output => output.amount))
+                    .toEqual([sendAmount, sendAmount]);
             });
         });
-
     });
 
-    describe('calculating balance', () => {
+    describe('calculating a balance', () => {
         let addBalance, repeatAdd, senderWallet;
 
         beforeEach(() => {
             senderWallet = new Wallet();
             addBalance = 100;
-            repeadAss = 3;
-
+            repeatAdd = 3;
             for (let i = 0; i < repeatAdd; i++) {
                 senderWallet.createTransaction(wallet.publicKey, addBalance, bc, tp);
             }
-
             bc.addBlock(tp.transactions);
         });
-
-        /*
-        it('calculates the balance for blockchain transactions matching the recipient', () => {
-            expect(wallet.calculateBalance(bc))
-                .toEqual(INITIAL_BALANCE + (addBalance * repeatAdd));
-        });
-
-        it('calculates the balance for blockchain transactions matching the sender', () => {
-            expect(wallet.calculateBalance(bc))
-                .toEqual(INITIAL_BALANCE - (addBalance * repeatAdd));
-        });
-        */
 
         it('calculates the balance for blockchain transactions matching the recipient', () => {
             expect(wallet.calculateBalance(bc))
@@ -87,6 +63,30 @@ describe('Wallet', () => {
                 .toEqual(INITIAL_BALANCE - (addBalance * repeatAdd));
         });
 
+        describe('and the recipient conducts a transaction', () => {
+            let subtractBalance, recipientBalance;
+
+            beforeEach(() => {
+                tp.clear();
+                subtractBalance = 60;
+                recipientBalance = wallet.calculateBalance(bc);
+                wallet.createTransaction(senderWallet.publicKey, subtractBalance, bc, tp);
+                bc.addBlock(tp.transactions);
+            });
+
+            describe('and the sender sends another transaction to the recipient', () => {
+                beforeEach(() => {
+                    tp.clear();
+                    senderWallet.createTransaction(wallet.publicKey, addBalance, bc, tp);
+                    bc.addBlock(tp.transactions);
+                });
+
+                it('calculate the recipient balance only using transactions since its most recent one', () => {
+                    expect(wallet.calculateBalance(bc))
+                        .toEqual(recipientBalance - subtractBalance + addBalance);
+                });
+            });
+        });
     });
-    
+
 });
